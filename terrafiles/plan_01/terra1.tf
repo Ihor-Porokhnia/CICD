4bfe8843394fd70d37cddb5fa6495bcc11283f2c
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-21 14:48:23 +0200

    removed credentionals

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  access_key = "AKIAZGWYTGJHOJ2FL7OK"
-  secret_key = "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"
-  region     = "eu-central-1"
-
 }
 resource "aws_instance" "myubuntu" {
   ami           = "ami-0cc0a36f626a4fdf5"

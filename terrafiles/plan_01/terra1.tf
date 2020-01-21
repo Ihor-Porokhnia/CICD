a96f789f0cd1f93954d79bbf216d84246ff63607
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-21 15:49:26 +0200

    reset

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-}
-resource "aws_instance" "my_ubuntu" {
+provider "aws" {}
+
+
+resource "aws_instance" "my_Ubuntu" {
   ami           = "ami-1dab2163"
   instance_type = "t2.micro"
-
 }

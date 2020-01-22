544186a6336556a504da1c2fc2fb1e37938a0e80
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-22 14:17:32 +0200

    vpc sec 3

--- terrafiles/plan_02/terra2.tf
+++ terrafiles/plan_02/terra2.tf
@@ -9,16 +9,16 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = "${aws_vpc.main_vps.id}"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    //cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    //cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {

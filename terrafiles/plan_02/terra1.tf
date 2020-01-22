888eabdaeb01f2d340e557709a89dcda9ae741ff
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-22 13:27:45 +0200

    rer

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -2,29 +2,32 @@ provider "aws" {}
 
 
 resource "aws_instance" "node1" {
-  ami           = "ami-1dab2163"
-  instance_type = "t3.micro"
-  key_name      = "main_key"
+  ami                         = "ami-1dab2163"
+  instance_type               = "t3.micro"
+  key_name                    = "main_key"
+  associate_public_ip_address = true
   network_interface {
     network_interface_id = "${aws_network_interface.int1.id}"
     device_index         = 1
   }
 }
 resource "aws_instance" "node2" {
-  ami           = "ami-1dab2163"
-  instance_type = "t3.micro"
-  key_name      = "main_key"
+  ami                         = "ami-1dab2163"
+  instance_type               = "t3.micro"
+  key_name                    = "main_key"
+  associate_public_ip_address = true
   network_interface {
     network_interface_id = "${aws_network_interface.int2.id}"
     device_index         = 1
   }
 }
 resource "aws_instance" "node3" {
-  ami           = "ami-1dab2163"
-  instance_type = "t3.micro"
-  key_name      = "main_key"
+  ami                         = "ami-1dab2163"
+  instance_type               = "t3.micro"
+  key_name                    = "main_key"
+  associate_public_ip_address = true
   network_interface {
     network_interface_id = "${aws_network_interface.int3.id}"
-    device_index         = 1
+    device_index         = 0
   }
 }

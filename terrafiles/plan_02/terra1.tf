350519ec19d25c250bd0a262af6a323abe8cd7d6
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-22 11:15:35 +0200

    terratest

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -5,4 +5,10 @@ resource "aws_instance" "nodes" {
 
   ami           = "ami-1dab2163"
   instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.int1.id}"
+    device_index         = 0
+  }
+
 }
--- terrafiles/plan_02/terra2.tf
+++ terrafiles/plan_02/terra2.tf
@@ -21,6 +21,27 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vps" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "default vps"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = "${aws_vpc.main_vps.id}"
+  cidr_block        = "172.31.0.0/24"
+  availability_zone = "eu-north-1c"
+
+  tags = {
+    Name = "internal subnet #1"
+  }
+}
+
+resource "aws_network_interface" "int1" {
+  subnet_id = "${aws_subnet.main_subnet.id}"
+
+  tags = {
+    Name = "primary_network_interface"
+  }
 }

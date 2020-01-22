6280b9848d9104e73e62d4e97ed8f43f34ba9a2d
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-22 10:50:26 +0200

    Update terra2.tf

--- terrafiles/plan_02/terra2.tf
+++ terrafiles/plan_02/terra2.tf
@@ -1,7 +1,25 @@
 provider "aws" {}
 
 
-resource "aws_instance" "my_Ubuntu" {
-  ami           = "ami-1dab2163"
-  instance_type = "t3.micro"
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]    
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]    
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
 }

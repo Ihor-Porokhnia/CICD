2ebe8218f14ea20f191125b6d81b974b3d86c19f
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-22 10:56:14 +0200

    remove duplicates

--- terrafiles/plan_02/terra2.tf
+++ terrafiles/plan_02/terra2.tf
@@ -1,22 +1,19 @@
-provider "aws" {}
-
-
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all traffic"
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {

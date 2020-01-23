2be31413bb4ee80a72babf62f5c773a7ad205122
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 16:29:19 +0200

    cloudtests

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -1,8 +1,3 @@
-provider "aws" {}
-
-
-resource "aws_instance" "my_Ubuntu" {
-  count         = 3
-  ami           = "ami-1dab2163"
-  instance_type = "t3.micro"
+provider "cloudflare" {
+  version = "~> 2.0"
 }

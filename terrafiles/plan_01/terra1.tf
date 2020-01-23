5372864bb6b5a0d5c2e2d6057cb2e9885c9fd3f0
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 20:56:00 +0200

    Update terra1.tf

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -5,3 +5,14 @@ resource "aws_instance" "my_Ubuntu" {
   ami           = "ami-1dab2163"
   instance_type = "t3.micro"
 }
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = "vpc-04be235a7dda36c60"
+  cidr_block              = "172.31.0.0/24"
+  availability_zone       = "eu-north-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "internal subnet #1"
+  }
+}

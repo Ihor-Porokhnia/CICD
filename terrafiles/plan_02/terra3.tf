f2f76beb3baebd51accc9a7665fe365cce2c86d7
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 17:17:37 +0200

    Update terra3.tf

--- terrafiles/plan_02/terra3.tf
+++ terrafiles/plan_02/terra3.tf
@@ -1,3 +1,13 @@
 provider "cloudflare" {
   version = "~> 2.0"
 }
+
+resource "cloudflare_record" "root" {
+  
+  count   = "${var.instance_count}"
+  zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
+  name    = "@"
+  value   = ${element(aws_instance.node.*.public_ip, count.index)}"
+  type    = "A"
+  proxied = false
+}

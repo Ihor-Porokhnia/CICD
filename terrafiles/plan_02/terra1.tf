7cbfeea7529dab9a4584cf7ba8bacd1c7ba43358
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 19:36:39 +0200

    Update terra1.tf

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -6,17 +6,17 @@ resource "aws_instance" "node" {
   ami             = "ami-1dab2163"
   instance_type   = "t3.micro"
   key_name        = "main_key"
-  subnet_id       = "${aws_subnet.main_subnet.id}"
-  private_ip      = "172.31.0.1${count.index}"
+  //subnet_id       = "${aws_subnet.main_subnet.id}"
+  //private_ip      = "172.31.0.1${count.index}"
   security_groups = ["${aws_security_group.allow_all.id}"]
 
+  
+  /*
   tags = {
     Name = "node.${count.index + 1}"
   }
-}
-
-output "instance_ips" {
-  value = ["${aws_instance.node.*.public_ip}"]
+  
+  */
 }
 
 

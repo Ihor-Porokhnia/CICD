f1178debbb645fb9f56f1f8fe0ce0abf09b515b6
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 19:39:01 +0200

    Update terra1.tf

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "node" {
   ami             = "ami-1dab2163"
   instance_type   = "t3.micro"
   key_name        = "main_key"
-  //subnet_id       = "${aws_subnet.main_subnet.id}"
+  subnet_id       = "${aws_subnet.main_subnet.id}"
   //private_ip      = "172.31.0.1${count.index}"
   security_groups = ["${aws_security_group.allow_all.id}"]
 

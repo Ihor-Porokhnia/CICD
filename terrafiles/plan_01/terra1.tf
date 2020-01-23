0c80c0fa5c4554ea5974fd3191f86c3a19dcee0c
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 16:34:10 +0200

    ter

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -1,3 +1,14 @@
 provider "cloudflare" {
   version = "~> 2.0"
 }
+variable "domain" {
+  default = "bugoga.ga"
+}
+
+resource "cloudflare_record" "www" {
+  domain  = "${var.domain}"
+  name    = "www"
+  value   = "203.0.113.10"
+  type    = "A"
+  proxied = false
+}

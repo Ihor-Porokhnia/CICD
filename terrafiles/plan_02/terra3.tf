4a086e745cedd5095482e7167916533b67f620f5
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 17:18:01 +0200

    Update terra3.tf

--- terrafiles/plan_02/terra3.tf
+++ terrafiles/plan_02/terra3.tf
@@ -7,7 +7,7 @@ resource "cloudflare_record" "root" {
   count   = "${var.instance_count}"
   zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
   name    = "@"
-  value   = ${element(aws_instance.node.*.public_ip, count.index)}"
+  value   = "${element(aws_instance.node.*.public_ip, count.index)}"
   type    = "A"
   proxied = false
 }

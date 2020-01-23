059897447d1b405aa978fa48d8833f28c9e941ce
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 16:41:39 +0200

    zone

--- terrafiles/plan_01/terra1.tf
+++ terrafiles/plan_01/terra1.tf
@@ -3,7 +3,8 @@ provider "cloudflare" {
 }
 
 resource "cloudflare_record" "www" {
-  domain  = "bugoga.ga"
+
+  zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
   name    = "www"
   value   = "203.0.113.10"
   type    = "A"

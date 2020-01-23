1bf361f03f168e6fd2afbad1187ee9b88765d003
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 13:25:08 +0200

    count index

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -2,12 +2,12 @@ provider "aws" {}
 
 resource "aws_instance" "node" {
   count           = 3
-  name            = "node.${i}"
+  name            = "node.${count.index}"
   ami             = "ami-1dab2163"
   instance_type   = "t3.micro"
   key_name        = "main_key"
   subnet_id       = "${aws_subnet.main_subnet.id}"
-  private_ip      = "172.31.0.1${i}"
+  private_ip      = "172.31.0.1${count.index}"
   security_groups = ["${aws_security_group.allow_all.id}"]
 }
 /*

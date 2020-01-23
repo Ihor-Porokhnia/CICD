5427114a8721300dac79d80dadf0806511061995
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-23 13:22:45 +0200

    ip change

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "node" {
   instance_type   = "t3.micro"
   key_name        = "main_key"
   subnet_id       = "${aws_subnet.main_subnet.id}"
-  private_ip      = ["172.31.0.1${i}"]
+  private_ip      = "172.31.0.1${i}"
   security_groups = ["${aws_security_group.allow_all.id}"]
 }
 /*

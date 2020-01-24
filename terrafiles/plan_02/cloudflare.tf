4fc21e551628cdc901f4234ed211aea479f40cbf
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-24 13:34:35 +0200

    3tst

--- terrafiles/plan_02/cloudflare.tf
+++ terrafiles/plan_02/cloudflare.tf
@@ -4,7 +4,7 @@ provider "cloudflare" {
 
 resource "cloudflare_record" "root" {
 
-  count   = "${var.instance_count}"
+  count   = 1 //"${var.instance_count}"
   zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
   name    = "@"
   value   = "${element(aws_instance.node.*.public_ip, count.index)}"
@@ -13,7 +13,7 @@ resource "cloudflare_record" "root" {
 }
 resource "cloudflare_record" "inst" {
 
-  count   = "${var.instance_count}"
+  count   = 1 //"${var.instance_count}"
   zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
   name    = "inst${count.index + 1}"
   value   = "${element(aws_instance.node.*.public_ip, count.index)}"

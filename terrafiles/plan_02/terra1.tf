3df62114113c01dc7e2707fc924bdf2227aa88fb
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-24 08:43:36 +0200

    Update terra1.tf

--- terrafiles/plan_02/terra1.tf
+++ terrafiles/plan_02/terra1.tf
@@ -1,76 +1,21 @@
 provider "aws" {}
 
-
-resource "aws_instance" "node1" {
-  //count           = 3
+resource "aws_instance" "node" {
+  count           = 3
   ami             = "ami-1dab2163"
   instance_type   = "t3.micro"
   key_name        = "main_key"
   subnet_id       = "${aws_subnet.main_subnet.id}"
-  //private_ip      = "172.31.0.1${count.index}"
-  security_groups = ["${aws_security_group.allow_all.id}"]
-
+  private_ip      = "172.31.0.1${count.index}"
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]  
   
-  /*
   tags = {
-    Name = "node.${count.index + 1}"
+    Name = "node${count.index + 1}"
   }
   
-  */
-}
-resource "aws_instance" "node2" {
-  //count           = 3
-  ami             = "ami-1dab2163"
-  instance_type   = "t3.micro"
-  key_name        = "main_key"
-  subnet_id       = "${aws_subnet.main_subnet.id}"
-  //private_ip      = "172.31.0.1${count.index}"
-  security_groups = ["${aws_security_group.allow_all.id}"]
-
   
-  /*
-  tags = {
-    Name = "node.${count.index + 1}"
-  }
-  
-  */
 }
 
-resource "aws_instance" "node3" {
-  //count           = 3
-  ami             = "ami-1dab2163"
-  instance_type   = "t3.micro"
-  key_name        = "main_key"
-  subnet_id       = "${aws_subnet.main_subnet.id}"
-  //private_ip      = "172.31.0.1${count.index}"
-  security_groups = ["${aws_security_group.allow_all.id}"]
-
-  
-  /*
-  tags = {
-    Name = "node.${count.index + 1}"
-  }
-  
-  */
-}
-
-
-
-/*
-resource "aws_instance" "node2" {
-  count           = "${var.instance_count}"
-  ami             = "ami-1dab2163"
-  instance_type   = "t3.micro"
-  key_name        = "main_key"
-  subnet_id       = "${aws_subnet.main_subnet.id}"
-  private_ip      = "172.31.0.2${count.index}"
-  security_groups = ["${aws_security_group.allow_all.id}"]
-
-  tags = {
-    Name = "lol.${count.index + 1}"
-  }
-}
-*/
 
 
 

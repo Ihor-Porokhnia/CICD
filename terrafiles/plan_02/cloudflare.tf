2c9da7b3b5d423cf0acdfc251cfa85bf8134a70a
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-01-24 09:33:12 +0200

    test cloud

--- terrafiles/plan_02/cloudflare.tf
+++ terrafiles/plan_02/cloudflare.tf
@@ -15,7 +15,7 @@ resource "cloudflare_record" "inst" {
 
   count   = "${var.instance_count}"
   zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
-  name    = "inst${count.index + 1}"
+  name    = "inst${count.index}"
   value   = "${element(aws_instance.node.*.public_ip, count.index)}"
   type    = "A"
   proxied = false

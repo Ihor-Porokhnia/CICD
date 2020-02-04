436a8f735b2de7cb3443bce65b271292bb735210
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-02-04 17:10:54 +0200

    Update terra1.tf

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -11,15 +11,5 @@ resource "google_container_cluster" "kubernetes" {
     password = ""
   }
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    tags = ["network-cluster"]
-  }
 }
 

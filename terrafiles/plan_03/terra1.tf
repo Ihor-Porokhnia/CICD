c66d5529a5e1f97848f85e11c2b5f3cc0ca7831d
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-02-04 16:03:43 +0200

    333

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -5,4 +5,11 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "n1-standard-1"
 
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
 }

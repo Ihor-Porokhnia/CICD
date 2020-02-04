b9344eed0d8f5aba9da5fe5af7354f695529d0d0
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-02-04 17:11:53 +0200

    Update terra1.tf

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -3,7 +3,6 @@ provider "google" {}
 
 resource "google_container_cluster" "kubernetes" {
   name               = "k8s-cluster"
-#  depends_on         = ["google_project_service.kubernetes"]
   initial_node_count = 1
 
   master_auth {

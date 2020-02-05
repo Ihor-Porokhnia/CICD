39005e6f25e24725e42236d76546bfcfb948d580
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-02-05 09:02:19 +0200

    112

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -1,13 +1,26 @@
 provider "google" {}
 
 
-resource "google_container_cluster" "kubernetes" {
-  name               = "k8s-cluster"
-  initial_node_count = 1
+resource "google_container_cluster" "primary" {
+  name = "k8s-epam"
+
+  initial_node_count = 2
 
   master_auth {
-    username = ""
-    password = ""
+    username = "admin"
+    password = "canttouchthis"
   }
 
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials k8s-epam"
+    }
 }

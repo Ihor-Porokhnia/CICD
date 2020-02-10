2dbdc03142c0c2c437abdb7efb717d269928e045
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-10 14:44:36 +0200

    add region

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -26,5 +26,6 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_compute_address" "k8s_address" {
-  name = "k8s-web-addr"
+  name   = "k8s-web-addr"
+  region = "eu-north-1"
 }

4d9ad581ea06d9f90f7e7e310d2288b75bfb3528
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-10 15:27:22 +0200

    213123

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -27,6 +27,6 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_compute_address" "k8s_address" {
+resource "google_compute_global_address" "k8s_address" {
   name   = "k8s-web-addr"
 }

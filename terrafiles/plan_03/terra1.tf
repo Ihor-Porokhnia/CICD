51331d567816e2dd7d5a5ef2f1291c811515c322
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-11 11:32:09 +0200

    ENV

--- terrafiles/plan_03/terra1.tf
+++ terrafiles/plan_03/terra1.tf
@@ -23,7 +23,7 @@ resource "google_container_cluster" "primary" {
   }
 
   provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials k8s-epam"
+    command = "export EXT_IP=${google_compute_address.k8s_address.address} && gcloud container clusters get-credentials k8s-epam"
   }
 }
 

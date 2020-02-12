2443daee3452c555b216894a2a6ec2a753d9bb34
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-12 16:21:25 +0200

    lang

--- terraforest/plan_03/cloudflare.tf
+++ terraforest/plan_03/cloudflare.tf
@@ -22,7 +22,7 @@ resource "cloudflare_record" "kubepoint" {
   proxied = false
 }
 
-resource "cloudflare_record" "ыйдзщште" {
+resource "cloudflare_record" "mysql" {
 
   zone_id = "cb31e9f86a3d9b6579e64701c6e4a95b"
   name    = "sql"

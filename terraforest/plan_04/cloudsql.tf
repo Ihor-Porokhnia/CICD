ae9f4f2b91c7d909777856ddba8520d3a81d36fb
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-13 09:43:55 +0200

    qwe

--- terraforest/plan_04/cloudsql.tf
+++ terraforest/plan_04/cloudsql.tf
@@ -2,12 +2,32 @@
 provider "google" {}
 
 resource "google_sql_database_instance" "master" {
-  name             = "master-huyaster"
+  name             = "master-suyaster"
   database_version = "MYSQL_5_7"
   region           = "europe-north1"
 
   settings {
     tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = "true"
+      authorized_networks {
+        value           = "0.0.0.0/0"
+        name            = "main"
+        
+      }
+    }
   }
+}
+
+resource "google_sql_database" "sd" {
+  name     = "SimpleDatabase"
+  instance = "${google_sql_database_instance.master.name}"
+}
+
+resource "google_sql_user" "remmie" {
+  name     = "remmie"
+  instance = "${google_sql_database_instance.master.name}"
+  host     = "%"
+  password = "nemA_666"
 }
 

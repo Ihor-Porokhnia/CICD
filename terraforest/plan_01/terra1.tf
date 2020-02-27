96469ef507140c1af634801bb8ee6a2d87f8d2ea
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-02-27 11:24:43 +0200

    terra test

--- terraforest/plan_01/terra1.tf
+++ terraforest/plan_01/terra1.tf
@@ -1,23 +1,23 @@
-provider "aws" {}
+provider "aws" {
+  region = "eu-north-1"
+  access_key = "${var.aws_id}"
+  secret_key = "${var.aws_secret}"
+}
 
 
 resource "aws_instance" "myUbuntu" {
-  count           = 3
+  
   ami           = "ami-1dab2163"
   instance_type = "t3.micro"
-  subnet_id       = "${aws_subnet.main_subnet.id}"
-  private_ip      = "172.31.0.1${count.index}"
-  vpc_security_group_ids = ["sg-09060a5daf6d556e3"]
+
 }
 
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = "vpc-04be235a7dda36c60"
-  cidr_block              = "172.31.0.0/24"
-  availability_zone       = "eu-north-1c"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "internal subnet #1"
-  }
+variable "aws_id" {
+  type    = string
+  default = "a"
 }
 
+variable "aws_secret" {
+  type    = string
+  default = "a"
+}

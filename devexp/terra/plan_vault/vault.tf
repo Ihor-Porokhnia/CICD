710630bc056e3c30cebd7d725a4f0684c646ee3e
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-04-30 11:26:35 +0300

    vvvvvvvvaaault

--- devexp/terra/plan_vault/vault.tf
+++ devexp/terra/plan_vault/vault.tf
@@ -2,3 +2,9 @@ provider "vault" {
  address = "${var.vault_addr}"
  token = "${var.vault_token}"
 }
+variable "vault_addr" {
+  type    = string  
+}
+variable "vault_token" {
+  type    = string  
+}

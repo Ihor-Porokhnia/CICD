b8ea262458b6c30f876f741844da3f86ee03cb8d
Ihor-Porokhnia <57176641+Ihor-Porokhnia@users.noreply.github.com>
2020-05-01 13:47:25 +0300

    Update aws.tf

--- devexp/terra/plan_vault/aws.tf
+++ devexp/terra/plan_vault/aws.tf
@@ -6,7 +6,7 @@ provider "aws" {
   access_key = data.vault_generic_secret.aws_secret.data["access_key"]
   secret_key = data.vault_generic_secret.aws_secret.data["secret_key"]
   token = data.vault_generic_secret.aws_secret.data["security_token"]
-  region  = "${var.region}"
+  region  = var.region
 }
 
 

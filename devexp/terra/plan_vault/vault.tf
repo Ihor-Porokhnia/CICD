22b0faeec5966501deaf35a511e02cadd53d5094
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-02 13:03:06 +0300

    vault rework

--- devexp/terra/plan_vault/vault.tf
+++ devexp/terra/plan_vault/vault.tf
@@ -3,16 +3,16 @@ provider "vault" {
  //token = var.vault_token
 }
 
-data "vault_generic_secret" "aws_secret" {
-  path = var.secret_path
-}
+//data "vault_generic_secret" "aws_secret" {
+// path = var.secret_path
+//}
 
-variable "vault_addr" {
-  type    = string  
-}
-variable "vault_token" {
-  type    = string  
-}
+//variable "vault_addr" {
+//  type    = string  
+//}
+//variable "vault_token" {
+//  type    = string  
+//}
 variable "secret_path" {
   type    = string  
 }

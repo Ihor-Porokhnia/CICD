ff7a67ac540f772384df159354a78845f15c848e
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-02 14:04:46 +0300

    sdf

--- devexp/terra/plan_vault/backend.tf
+++ devexp/terra/plan_vault/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
+    token = "yMofyIDRtjCkiA.atlasv1.8cCnT3KzARYR3lez4g82TheJawFa9K8RWRtfoJpNFiTPqyCaelIYGgpZdvCnXAYZskA"
     organization = "iptools"
 
     workspaces {

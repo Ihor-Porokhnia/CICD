a1b43e65f492eb2b43e0bf41b6e92b975c2ac1e2
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-02 14:52:38 +0300

    qweqwe

--- devexp/terra/plan_vault/vault.tf
+++ devexp/terra/plan_vault/vault.tf
@@ -1,18 +1,9 @@
-provider "vault" {
- //address = var.vault_addr
- //token = var.vault_token
-}
+provider "vault" {}
 
 data "vault_generic_secret" "aws_secret" {
  path = var.secret_path
 }
 
-//variable "vault_addr" {
-//  type    = string  
-//}
-//variable "vault_token" {
-//  type    = string  
-//}
 variable "secret_path" {
   type    = string  
 }

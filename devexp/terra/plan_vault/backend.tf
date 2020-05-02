04fac6aaae9f0998b46d5595a1991d1e49ac542f
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-02 14:12:44 +0300

    sdas

--- devexp/terra/plan_vault/backend.tf
+++ devexp/terra/plan_vault/backend.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    token = "yMofyIDRtjCkiA.atlasv1.8cCnT3KzARYR3lez4g82TheJawFa9K8RWRtfoJpNFiTPqyCaelIYGgpZdvCnXAYZskA"
+    
     organization = "iptools"
 
     workspaces {
       name = "remo01"
     }
+    token = "yMofyIDRtjCkiA.atlasv1.8cCnT3KzARYR3lez4g82TheJawFa9K8RWRtfoJpNFiTPqyCaelIYGgpZdvCnXAYZskA"
   }
 }

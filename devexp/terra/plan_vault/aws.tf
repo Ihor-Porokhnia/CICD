54f9c529e33528add34bbf292c224ab38e31600b
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-04 15:34:02 +0300

    asd

--- devexp/terra/plan_vault/aws.tf
+++ devexp/terra/plan_vault/aws.tf
@@ -52,54 +52,6 @@ resource "aws_iam_role" "beanstalk_service" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role" "beanstalk_ec2" {
-    name = "beanstalk-ec2-role"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "beanstalk_service" {
-    name = "beanstalk-service-user"
-    roles = ["${aws_iam_role.beanstalk_service.name}"]
-}
-
-resource "aws_iam_instance_profile" "beanstalk_ec2" {
-    name = "beanstalk-ec2-user"
-    roles = ["${aws_iam_role.beanstalk_ec2.name}"]
-}
-
-resource "aws_iam_role" "beanstalk_service" {
-    name = "beanstalk-service-role"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
       "Sid": "",
       "Effect": "Allow",
       "Principal": {

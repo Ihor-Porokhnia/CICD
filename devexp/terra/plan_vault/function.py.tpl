b5b158c544156eb3a1b7694d7d506ff08d61638e
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-10 13:26:59 +0300

    tpl

---
+++ devexp/terra/plan_vault/function.py.tpl
@@ -0,0 +1,23 @@
+import boto3, json
+
+def lambda_handler(event, context):
+    app_name="${APPNAME}"
+    env_id="${ENVID}"
+    client = boto3.client('elasticbeanstalk')
+    try:
+     response = client.update_environment(
+         ApplicationName=app_name,
+         EnvironmentId=env_id,
+         VersionLabel=event['app_version']
+      )
+     return (str(response))
+    except Exception as e:
+     exception_type = e.__class__.__name__
+     exception_message = str(e)
+     api_exception_obj = {
+      "isError": True,
+      "type": exception_type,
+      "message": exception_message
+     }
+     api_exception_json = json.dumps(api_exception_obj)
+     return(api_exception_json)
---
+++ devexp/terra/plan_vault/lambdaresources.tf
@@ -0,0 +1,26 @@
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "function.zip"
+  function_name    = "${var.project_name}-lambda"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_handler"
+  source_code_hash = filebase64sha256("function.zip")
+  runtime          = "python3.8"
+}
+
+
+data "template_file" "function" {
+  template = file("function.py.tpl")
+  vars = {
+    APPNAME = aws_elastic_beanstalk_application.beanapp.name
+    ENVID   = aws_elastic_beanstalk_environment.api.id
+  }
+}
+
+data "archive_file" "lambda_zip" {
+  type = "zip"
+  source {
+    content  = data.template_file.function.rendered
+    filename = "function.py"
+  }
+  output_path = "function.zip"
+}

6706d2bf2c6710dccd084f84549ccf9a6bc4ac2e
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-10 11:37:35 +0300

    1232113

--- devexp/terra/plan_vault/aws.tf
+++ devexp/terra/plan_vault/aws.tf
@@ -141,7 +141,7 @@ resource "aws_iam_role" "beanstalk_ec2" {
 EOF
 }
 
-resource "aws_iam_role" "lambda" {
+resource "aws_iam_role" "lambda_role" {
   name               = "${var.project_name}-lambda-role"
   assume_role_policy = <<EOF
 {
@@ -160,9 +160,9 @@ resource "aws_iam_role" "lambda" {
 EOF
 }
 
-resource "aws_iam_role_policy" "test_policy" {
+resource "aws_iam_role_policy" "lambda-cloudwatch-policy" {
   name = "${var.project_name}-lambda-cloudwatch-policy"
-  role = aws_iam_role.lambda.id
+  role = aws_iam_role.lambda_role.id
 
   policy = jsonencode({
 
@@ -186,7 +186,10 @@ resource "aws_iam_role_policy" "test_policy" {
     ]
   })
 }
-
+resource "aws_iam_role_policy_attachment" "lambda_beanstalk_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkFullAccess"
+}
 
 resource "aws_iam_role_policy_attachment" "beanstalk_service" {
   role       = aws_iam_role.beanstalk_service.name

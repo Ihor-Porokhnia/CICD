0d938f4007e629584fac1c6f453723d7e8f776c3
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-11 11:43:12 +0300

    1221

--- devexp/terra/plan_vault/apigatewayresourses.tf
+++ devexp/terra/plan_vault/apigatewayresourses.tf
@@ -46,3 +46,8 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "dev"
 }
+resource "aws_api_gateway_stage" "test" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+}

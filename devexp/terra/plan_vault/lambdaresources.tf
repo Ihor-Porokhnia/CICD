c3b0b35fc175999b1d8a2814138d33b56844a3b2
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-12 16:40:29 +0300

    123

--- devexp/terra/plan_vault/lambdaresources.tf
+++ devexp/terra/plan_vault/lambdaresources.tf
@@ -40,7 +40,7 @@ data "archive_file" "lambda_zip" {
 
 data "aws_lambda_invocation" "update_ver_invoke" {
   function_name = aws_lambda_function.lambda.function_name
-  input =  jsonencode({"hello"="world"})
+  input =  "{ \"app_version\": \"api-test-1-ssl-test-jenkins-EBS-48.zip\" }"
 }
 
       

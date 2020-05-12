06099ae27959dfd52805127ee19932b496bda0e3
Ihor-Porokhnia <ihor.porokhnia@gmail.com>
2020-05-12 17:29:24 +0300

    qweqweqwe

--- devexp/terra/plan_vault/lambdaresources.tf
+++ devexp/terra/plan_vault/lambdaresources.tf
@@ -50,6 +50,6 @@ resource "null_resource" "null" {
   
   triggers = {
     invoke = data.aws_lambda_invocation.update_ver_invoke
-    input = "{\"asddsa\": \"asas\"}"
+    input = "\{\"asddsa\": \"asas\"\}"
   }
 }

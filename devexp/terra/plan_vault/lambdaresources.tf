32f3b69e07d83ca80b0e87c780bfe9a6ee1848b5
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-13 13:30:16 +0300

    2222222

--- devexp/terra/plan_vault/lambdaresources.tf
+++ devexp/terra/plan_vault/lambdaresources.tf
@@ -40,12 +40,12 @@ data "archive_file" "lambda_zip" {
 
 data "aws_lambda_invocation" "update_ver_invoke" {
   function_name = aws_lambda_function.lambda.function_name
-  input = <<EOF
-{
-  "app_version": "api-test-1-ssl-test-jenkins-EBS-48.zip"  
-}
-EOF
-  depends_on = [aws_lambda_function.lambda]
+  input = jsonencode({
+  "app_version"="api-test-1-ssl-test-jenkins-EBS-48.zip"  
+   })
+  depends_on = [aws_lambda_function.lambda,
+  aws_elastic_beanstalk_application.beanapp,
+  aws_elastic_beanstalk_environment.api]
 }
 
       

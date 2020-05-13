7546ea5b58dc148f3a5ca6efa3d761aa00d8dd1e
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-13 16:21:54 +0300

    112223333

--- devexp/terra/plan_vault/route53resources.tf
+++ devexp/terra/plan_vault/route53resources.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "elbstalk" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
+  zone_id = "Z0909442AOAL213NZWY3"
   name    = "ebs3.devexp.gq"
   type    = "A"
 
   alias {
-    name                   = aws_elastic_beanstalk_application_version.default.load_balancers[1].dns_name
-    zone_id                = aws_elastic_beanstalk_application_version.default.load_balancers[1].zone_id
+    name                   = aws_elastic_beanstalk_application_version.default.load_balancers.dns_name
+    zone_id                = aws_elastic_beanstalk_application_version.default.load_balancers.zone_id
     evaluate_target_health = true
   }
 }

1abe76d0078151f8b3c8531573ef2723aa722043
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-14 09:05:18 +0300

    ebs hosted zone

--- devexp/terra/plan_vault/route53resources.tf
+++ devexp/terra/plan_vault/route53resources.tf
@@ -1,11 +1,12 @@
-/* resource "aws_route53_record" "elbstalk" {
+resource "aws_route53_record" "elbstalk" {
   zone_id = "Z0909442AOAL213NZWY3"
   name    = "ebs3.devexp.gq"
   type    = "A"
 
   alias {
-    name                   = aws_elastic_beanstalk_application_version.default.load_balancers.dns_name
-    zone_id                = aws_elastic_beanstalk_application_version.default.load_balancers.zone_id
+    name                   = aws_elastic_beanstalk_environment.api.cname
+    zone_id                = data.aws_elastic_beanstalk_hosted_zone.current.id
     evaluate_target_health = true
   }
-} */
+} 
+data "aws_elastic_beanstalk_hosted_zone" "current" {}

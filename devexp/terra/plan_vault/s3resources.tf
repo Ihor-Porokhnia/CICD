ad48cc8d718a26614ac86f6ad1e4fb17cc8ab386
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-14 10:28:02 +0300

    stor

--- devexp/terra/plan_vault/s3resources.tf
+++ devexp/terra/plan_vault/s3resources.tf
@@ -12,7 +12,24 @@ resource "aws_s3_bucket_object" "artifact" {
   bucket     = aws_s3_bucket.backend_S3_bucket.id
   source     = "${var.local_path}/${var.artifact_name}"
   etag       = filemd5("${var.local_path}/${var.artifact_name}")
+  depends_on = [aws_s3_bucket_object.upexist]
 }
+
+data "aws_s3_bucket_objects" "existing_objects" {
+  bucket = "${var.project_name}--bucket"
+  prefix =  var.project_name
+}
+
+/* data "aws_s3_bucket_object" "existing_object_info" {
+  count  = length(data.aws_s3_bucket_objects.existing_objects.keys)
+  key    = element(data.aws_s3_bucket_objects.existing_objects.keys, count.index)  
+} */
+resource "aws_s3_bucket_object" "upexist" {
+  count      = length(data.aws_s3_bucket_objects.existing_objects.keys)
+  key        = element(data.aws_s3_bucket_objects.existing_objects.keys, count.index)
+  bucket     = aws_s3_bucket.backend_S3_bucket.id
+}
+
 variable "artifact_name" {
   type    = string  
 }

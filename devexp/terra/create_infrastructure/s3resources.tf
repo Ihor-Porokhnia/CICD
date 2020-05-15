36942bbd66903ddcc3fdc63043d91daee47c9cec
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-15 12:18:00 +0300

    force_destroy

--- devexp/terra/create_infrastructure/s3resources.tf
+++ devexp/terra/create_infrastructure/s3resources.tf
@@ -4,5 +4,6 @@ This plan used 2 create S3 bucket and upload artifacts
 resource "aws_s3_bucket" "backend_S3_bucket" {
   bucket = "${var.project_name}--bucket"
   acl    = "private"
-  region = var.region  
+  region = var.region
+  force_destroy = true  
 }

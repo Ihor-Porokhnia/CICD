dfd28ee98c0be6bfee87d8b1a8e63892564d58f7
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-15 12:35:04 +0300

    data

---
+++ devexp/terra/delivery_artifact/lambdaresources.tf
@@ -0,0 +1,27 @@
+/*
+This plan generates python lambda from template, zips it into archive and uploads it
+*/
+
+ data "aws_lambda_invocation" "create_version" {
+  function_name = "${var.project_name}-lambda-beanstalk-control"
+  input = jsonencode({
+  "operation"="create"  
+  "app_version"=var.artifact_name 
+   })
+  depends_on = [aws_s3_bucket_object.artifact]
+}
+
+data "aws_lambda_invocation" "set_version" {
+  function_name = "${var.project_name}-lambda-beanstalk-control"
+  input = jsonencode({
+  "operation"="create"  
+  "app_version"=var.artifact_name 
+   })
+  depends_on = [aws_s3_bucket_object.artifact, data.aws_lambda_invocation.set_version]
+}
+output "lambda_create_output" {
+  value = jsondecode(data.aws_lambda_invocation.create_version.result)["params"]
+} 
+output "lambda_set_output" {
+  value = jsondecode(data.aws_lambda_invocation.set_version.result)["params"]
+} 

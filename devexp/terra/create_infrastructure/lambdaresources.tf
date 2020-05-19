df2c9162788f2c6d34bfa4cb78f2d6eece885993
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-19 13:08:02 +0300

    www

--- devexp/terra/create_infrastructure/lambdaresources.tf
+++ devexp/terra/create_infrastructure/lambdaresources.tf
@@ -19,7 +19,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com" 
-  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 data "template_file" "function" {

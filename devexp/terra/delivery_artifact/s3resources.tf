e53403794c2bc2e5891aedec76fdf0c1f13792e8
Ihor-Porokhnia <porokhnya.igor@gmail.com>
2020-05-19 13:17:02 +0300

    1111

--- devexp/terra/delivery_artifact/s3resources.tf
+++ devexp/terra/delivery_artifact/s3resources.tf
@@ -1,18 +1,13 @@
 /*
 This plan used 2 create S3 bucket and upload artifacts
 */
-/* resource "aws_s3_bucket" "backend_S3_bucket" {
-  bucket = "${var.project_name}--bucket"
-  acl    = "private"
-  region = var.region  
-} */
+
 
 resource "aws_s3_bucket_object" "artifact" {
   key        = "${var.upload_s3_prefix}/${var.artifact_name}"
   bucket     = "${var.project_name}--bucket"
   source     = "${var.local_path}/${var.artifact_name}"
-  etag       = filemd5("${var.local_path}/${var.artifact_name}")
-  
+  etag       = filemd5("${var.local_path}/${var.artifact_name}")  
 }
 
 
